e4c7a0dd89202c2fe8dbe128b3195b464234b7a1
ehoneahobed <ehoneahobed@hotmail.com>
2024-03-22 15:14:28 -0400

    Initial commit with basic functionality

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,228 @@
+"use client"
+
 import Image from "next/image";
+import axios from 'axios';
+import { useEffect, useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+import { Button } from "@/components/ui/button"
+import { Badge } from "@/components/ui/badge"
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
+import { Skeleton } from "@/components/ui/skeleton"
+
+
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState({ count: 0, entries: [] });
+
+  // states for pagination
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(9);
+
+  // search feature
+  const [ searchQuery, setSearchQuery ] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const results = await axios.get("https://api.publicapis.org/entries");
+        // console.log(results.data.count);
+        // setData(results.data.entries);
+        setData({ count: results.data.count, entries: results.data.entries });
+
+        setLoading(false)
+        // console.log(data)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  // calculating the pagination data
+  const totalPages = Math.ceil(data.count / itemsPerPage);
+  // console.log(totalPages)
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.entries.slice(indexOfFirstItem, indexOfLastItem);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  // a function to render pagination
+  // const renderPaginationItems = () => {
+  //   console.log(totalPages)
+  //   let items = [];
+  //   for (let number = 1; number <= totalPages; number++) {
+  //     items.push(
+  //       <PaginationItem key={number} active={number === currentPage}>
+  //         <PaginationLink href="#" onClick={(e) => handlePageChange(e, number)} className="bg-white">
+  //           {number}
+  //         </PaginationLink>
+  //       </PaginationItem>
+  //     );
+  //   }
+
+  //   // Example logic to insert an ellipsis for a large number of pages
+  //   if (totalPages > 5) {
+  //     const startItems = items.slice(0, 2);
+  //     const endItems = items.slice(-2);
+  //     items = [...startItems, <PaginationEllipsis className="bg-white" />, ...endItems];
+  //   }
+
+  //   console.log(items)
+  //   return items;
+  // };
+
+  const renderPaginationItems = () => {
+    let items = [];
+    let leftSide = currentPage - 2;
+    let rightSide = currentPage + 2;
+  
+    for (let number = 1; number <= totalPages; number++) {
+      if (number === 1 || number === totalPages || (number >= leftSide && number <= rightSide)) {
+        items.push(
+          <PaginationItem key={number} active={number === currentPage}>
+            <PaginationLink href="#" onClick={(e) => handlePageChange(e, number)} className="bg-white">
+              {number}
+            </PaginationLink>
+          </PaginationItem>
+        );
+      }
+    }
+  
+    // Logic to add ellipses where there are skipped page numbers
+    const paginationItemsWithEllipses = [];
+    let previousItem = null;
+    for (const item of items) {
+      const pageNumber = parseInt(item.key);
+      if (previousItem) {
+        const previousPageNumber = parseInt(previousItem.key);
+        if (pageNumber - previousPageNumber === 2) {
+          // If there's only one page number between the current and the previous item, add that page number instead of an ellipsis
+          paginationItemsWithEllipses.push(
+            <PaginationItem key={previousPageNumber + 1}>
+              <PaginationLink href="#" onClick={(e) => handlePageChange(e, previousPageNumber + 1)} className="bg-white">
+                {previousPageNumber + 1}
+              </PaginationLink>
+            </PaginationItem>
+          );
+        } else if (pageNumber - previousPageNumber !== 1) {
+          // If there's more than one page number between the current and the previous item, add an ellipsis
+          paginationItemsWithEllipses.push(<PaginationEllipsis className="bg-white" />);
+        }
+      }
+      paginationItemsWithEllipses.push(item);
+      previousItem = item;
+    }
+  
+    return paginationItemsWithEllipses;
+  };
+  
+
+  // handle page change
+  const handlePageChange = (e, number) => {
+    e.preventDefault();
+    setCurrentPage(number);
+  };
+
+  // handling user search
+  // const filteredData = data.filter(entry =>
+  //   entry.API.toLowerCase().includes(searchQuery.toLowerCase())
+  // );
+  
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    // <main className="flex min-h-screen flex-wrap items-center justify-between p-24">
+    <main className="flex flex-col min-h-screen bg-slate-900  p-24">
+      <h2 className="text-white font-bold text-3xl mb-8">Publicly Available APIs</h2>
+      <div className="flex justify-end mb-4">
+      <input
+  type="text"
+  placeholder="Search APIs..."
+  value={searchQuery}
+  onChange={(e) => setSearchQuery(e.target.value)}
+  className="search-input py-3 px-10"
+/>
+
+      </div>
+      {loading ? (
+        <div className="grid md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 gap-4">
+          <Skeleton />
+          <Skeleton />
+          <Skeleton className="h-[200px] w-[400px] rounded-xl" />
+          <Skeleton />
+          <Skeleton />
+          <Skeleton className="h-[200px] w-[400px] rounded-xl" />
+          <Skeleton />
+          <Skeleton />
+          <Skeleton className="h-[200px] w-[400px] rounded-xl" />
         </div>
+      ) : (<div className="grid md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 gap-4">
+
+        {currentItems.map((entry, index) => (
+          <Card key={entry.API}>
+            <CardHeader>
+              <CardTitle>{entry.API}</CardTitle>
+              <CardDescription>
+                <div className="flex gap-3">
+                  {entry.Auth !== "" ? (<p>Auth: {entry.Auth}</p>) : ""}
+                  {entry.Cors !== "yes" ? (<p>Cors: No</p>) : (<p>Cors: Yes</p>)}
+                  {entry.HTTPS === true ? (<p>HTTPS: True</p>) : ""}
+                </div>
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>{entry.Description}</p>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <Button variant="outline" className='bg-black text-white' onClick={() => { window.open(entry.Link, "_blank") }}>Visit</Button>
+              <Badge variant="outline">{entry.Category}</Badge>
+            </CardFooter>
+          </Card>
+        ))}
       </div>
+      )}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {!loading ? (<Pagination className="mt-8">
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious
+              href="#"
+              onClick={(e) => handlePageChange(e, currentPage - 1)}
+              disabled={currentPage === 1}
+              className="bg-white"
+            />
+          </PaginationItem>
+          {renderPaginationItems()}
+          <PaginationItem>
+            <PaginationNext
+              href="#"
+              onClick={(e) => handlePageChange(e, currentPage + 1)}
+              disabled={currentPage === totalPages}
+              className="bg-white"
+            />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+      ) : ("")}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
